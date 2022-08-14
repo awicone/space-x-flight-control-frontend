1e02e488fb03f5e90880fbd2fb004aa3ed979ffd
awicone <awiconet@gmail.com>
2022-08-14 09:59:27 +0300

    update webpack.config, add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import s from './App.module.css';
 
 const App = () => {
   return (
-    <div>
+    <div className={s.container}>
       <h1>Hello!!</h1>
       <h2>Welcome to SpaceX flying control center..!</h2>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,52 @@
 const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: prod ? 'production' : 'development',
   entry: './src/index.tsx',
   output: {
-    path: __dirname + '/dist/',
+    path: __dirname + '/dist/'
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader']
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'ts-loader',
+        use: 'ts-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        }
       },
       {
         test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ["file-loader"],
-      },
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        type: 'asset/resource'
+      }
     ]
   },
   devtool: prod ? undefined : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
-    })
-  ],
+      template: 'index.html'
+    }),
+    new ESLintPlugin()
+  ]
 };
