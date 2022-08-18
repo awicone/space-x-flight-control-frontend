677b2d96456afc0a1b28ca627622435a4242b919
awicone <awiconet@gmail.com>
2022-08-18 03:24:05 +0300

    Add Procfile

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const baseURL = process.env.APP_ENV === 'DEV' ?
+const JSON_SERVER_API_URL = process.env.APP_ENV === 'DEV' ?
   'http://localhost:8000' :
-  'https://space-x-flight-control-app.herokuapp.com:8000';
+  process.env.APP_JSON_API_URL;
 
 const backend = axios.create({
   baseURL: process.env.APP_LAUNCHES_URL
 });
 
 const jsonServer = axios.create({
-  baseURL
+  baseURL: JSON_SERVER_API_URL
 });
 
 export { backend, jsonServer };
